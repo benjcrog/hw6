3b23052843486db29b1307b0c135ba01231179f0
Ben Rogers <benrogers@bens-mbp.attlocal.net>
2018-10-25 16:07:53 -0400

    Final function finished (listURLs)

--- hw6.py
+++ hw6.py
@@ -33,32 +33,41 @@ def countWord(fileName, word):
     return(lenOfList)
 
 #
-# def listURLs(fileName):
-#     pass
+def listURLs(fileName):
+    urlsList =[]
+    f = open(fileName)
+    for line in f:
+        line = line.rstrip()
+        url = re.findall("w{3}\.[a-zA-Z0-9_.+-]+\.[a-zA-Z0-9-.]+", line)
+        print(url)  
+        urlsList = urlsList + url
+    return urlsList
 
 
 #print(sumNums("regex_sum_42.txt"))
-print(countWord("regex_sum_42.txt", "computer"))
+#print(countWord("regex_sum_42.txt", "computer"))
+#print(listURLs("regex_sum_42.txt"))
+#     python hw6.py
 
 
-# class TestHW6(unittest.TestCase):
-#     """ Class to test this homework """
-#
-#     def test_sumNums1(self):
-#         """ test sumNums on the first file """
-#         self.assertEqual(sumNums("regex_sum_42.txt"), 445833)
-#
-#     def test_sumNums2(self):
-#         """ test sumNums on the second file """
-#         self.assertEqual(sumNums("regex_sum_132198.txt"), 374566)
-#
-#     def test_countWord(self):
-#         """ test count word on the first file """
-#         self.assertEqual(countWord("regex_sum_42.txt", "computer"),21)
-#
-#     def test_listURLs(self):
-#         """ test list URLs on the first file """
-#         self.assertEqual(len(listURLs("regex_sum_42.txt")), 3)
-#
-# # run the tests
-# unittest.main(verbosity=2)
+class TestHW6(unittest.TestCase):
+    """ Class to test this homework """
+
+    def test_sumNums1(self):
+        """ test sumNums on the first file """
+        self.assertEqual(sumNums("regex_sum_42.txt"), 445833)
+
+    def test_sumNums2(self):
+        """ test sumNums on the second file """
+        self.assertEqual(sumNums("regex_sum_132198.txt"), 374566)
+
+    def test_countWord(self):
+        """ test count word on the first file """
+        self.assertEqual(countWord("regex_sum_42.txt", "computer"),21)
+
+    def test_listURLs(self):
+        """ test list URLs on the first file """
+        self.assertEqual(len(listURLs("regex_sum_42.txt")), 3)
+
+# run the tests
+unittest.main(verbosity=2)
